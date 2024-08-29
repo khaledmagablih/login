1958273b46448bcabe34dbd36693e32e04e9610c
Khaled A. Magablih <80848677+khaledmagablih@users.noreply.github.com>
2024-08-29 20:17:21 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { UserProvider } from './contexts/UserContext';
 import Login from './Login';
 import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <UserProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Router>
-    </UserProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,17 +1,16 @@
-// src/Dashboard.js
-import React, { useContext } from 'react';
-import { UserContext } from './contexts/UserContext';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Dashboard() {
-  const { user } = useContext(UserContext);
+  const { email, password } = useSelector((state) => state.auth);
 
   return (
     <div className="flex justify-center items-center min-h-screen bg-gray-100">
       <div className="bg-white p-10 rounded-lg shadow-lg w-96 text-center">
         <h1 className="text-2xl font-bold text-gray-800">Dashboard</h1>
         <p className="text-gray-600 mt-2 mb-5 text-base">Welcome to your dashboard!</p>
-        <p><strong>Email:</strong> {user.email}</p>
-        <p><strong>Password:</strong> {user.password}</p>
+        <p><strong>Email:</strong> {email}</p>
+        <p><strong>Password:</strong> {password}</p>
       </div>
     </div>
   );
